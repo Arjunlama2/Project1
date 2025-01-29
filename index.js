02db422cd73ad2ffe9ca8faee0e127139a03510f
Arjunlama2 <instructorarjun1@gmail.com>
2025-01-29 14:06:59 +0545

    javascript introduction

---
+++ index.js
@@ -0,0 +1,49 @@
+
+// value 
+// String, number ,Boolean
+
+// variable
+//  empty space to store value
+//  we can create variable by using aplbets
+ 
+
+// var firstNumber=5
+// var secondNumber=6
+
+// var ans=firstNumber+secondNumber
+
+
+// console.log("answer of addition",ans)
+
+
+// operators
+//  arithematic operators
+//             + - * / \ %
+//   assignmen operator
+//    =
+
+// comparetive operator
+//     < > <= >= == != it gives up boolean value
+
+
+// task
+// wap to add , subtract, multiply and divide two number
+// wap to find the remainder when divided
+//  let a=6
+//  let b=2
+ 
+// if (a<b){
+// console.log("a is smaller")
+// }else{
+// console.log(b," is smaller than",a)
+// }
+
+
+// wap to determine positive or not
+
+let number =5
+
+
+
+
+
