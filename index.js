ab825e5805338168e98f233fcb8f86d853eb50dc
Arjunlama2 <instructorarjun1@gmail.com>
2025-01-31 14:03:49 +0545

    js functions

--- index.js
+++ index.js
@@ -41,12 +41,94 @@
 
 // wap to determine positive or not
 
-let number =5
+// let number =5
+
+
+// function declaration
+
+
+
+ function add (first,second){
+let c=first+second
+return c
+ }
+
+
+//  function subtract (first,second){
+//     let c=first-second
+//     console.log(c)
+//      }
+
+
+     
+//  function multiply(first,second){
+//     let c=first*second
+//     console.log(c)
+//      }
 
 
 
 
+//      add(80,90)
+
+//      subtract(80,1)
+
+//      add (90,1)
+
+
+// function checkpositive(number){
+
+// if(number== 0){
+//     console.log("Zero")
+// }else if(number<0){
+//     console.log("negative")
+// }else{
+//     console.log("Positive")
+// }
+//  return 65
+// }
+
+
+
+// let value=checkpositive(0)
+// let sum=add(9,8)
+
+// // console.log("return value",value)
+// console.log(sum)
+
+
+
+// function covertTosecond(min){
+//  let sec= min*60
+
+//  return sec
+// }
 
 
 
 
+// let second=covertTosecond(8)
+
+
+// console.log("converted to second",covertTosecond(10))
+
+
+
+// create a function that takes two different numbers and return the greatest among them
+
+
+
+function checkGreatest(first,second){
+    if(first>second){
+        return  first
+    }
+    else{
+        return second
+    }
+}
+
+
+let greatest=checkGreatest(5,8)
+
+
+console.log("greatest is ",greatest)
